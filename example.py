8699d6af58cdeee2dbbe04492303c3bdea5aa4ab
adi.a <adi.a@solocate.com>
2022-07-25 07:20:48 +0300

    change code to be be a function instead of a script

--- example.py
+++ example.py
@@ -15,60 +15,74 @@ parser.add_argument('--data', metavar='IMAGENETVALDIR', help='path to Imagenet V
 parser.add_argument('--batch_size', metavar='BSZ', help='batch size', default=128)
 parser.add_argument('--num_workers', metavar='NW', help='number of workers', default=0)
 parser.add_argument('--num_calib', metavar='NCALIB', help='number of calibration points', default=10000)
-parser.add_argument('--seed', metavar='SEED', help='random seed', default=0)
+# parser.add_argument('--seed', metavar='SEED', help='random seed', default=0)
+# optimize for 'size' or 'adaptiveness'
+lamda_criterion = 'size'
 
-if __name__ == "__main__":
+def main(seed=0,kreg = None,constant_regularization = True,lamda=None):
     args = parser.parse_args()
-    ### Fix randomness 
-    np.random.seed(seed=args.seed)
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed(args.seed)
-    random.seed(args.seed)
+    ### Fix randomness
+    np.random.seed(seed=seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    random.seed(seed)
 
-    # Transform as in https://github.com/pytorch/examples/blob/42e5b996718797e45c46a25c55b031e6768f8440/imagenet/main.py#L92 
+    # Transform as in https://github.com/pytorch/examples/blob/42e5b996718797e45c46a25c55b031e6768f8440/imagenet/main.py#L92
     transform = transforms.Compose([
-                    transforms.Resize(256),
-                    transforms.CenterCrop(224),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std= [0.229, 0.224, 0.225])
-                ])
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
 
     # Get the conformal calibration dataset
-    imagenet_calib_data, imagenet_val_data = torch.utils.data.random_split(torchvision.datasets.ImageFolder(args.data, transform), [args.num_calib,50000-args.num_calib])
+    imagenet_calib_data, imagenet_val_data = torch.utils.data.random_split(
+        torchvision.datasets.ImageFolder(args.data, transform), [args.num_calib, 50000 - args.num_calib])
 
-    # Initialize loaders 
-    calib_loader = torch.utils.data.DataLoader(imagenet_calib_data, batch_size=args.batch_size, shuffle=True, pin_memory=True)
-    val_loader = torch.utils.data.DataLoader(imagenet_val_data, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+    # Initialize loaders
+    calib_loader = torch.utils.data.DataLoader(imagenet_calib_data, batch_size=args.batch_size, shuffle=True,
+                                               pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(imagenet_val_data, batch_size=args.batch_size, shuffle=True,
+                                             pin_memory=True)
 
     cudnn.benchmark = True
 
     # Get the model
-    #define gpu
-    os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+    # define gpu
+    os.environ["CUDA_VISIBLE_DEVICES"] = '2'
     device = torch.device("cuda:0")
 
-    model = torchvision.models.resnet152(pretrained=True,progress=True).to(device)
-    model = torch.nn.DataParallel(model) 
+    model = torchvision.models.resnet152(pretrained=True, progress=True).to(device)
+    model = torch.nn.DataParallel(model)
     model.eval()
 
     # if None, model will pick lamda on its own
-    lamda = None
-    #define
-    kreg = None
+    # lamda = None
+    # define
+    # kreg = None
     # use the normal RAPS regularization
-    constant_regularization = False
+    # constant_regularization = True
     # optimize for 'size' or 'adaptiveness'
-    lamda_criterion = 'size'
+    # lamda_criterion = 'size'
     # allow sets of size zero
-    allow_zero_sets = False 
+    allow_zero_sets = False
     # use the randomized version of conformal
-    randomized = True 
+    randomized = True
 
     # Conformalize model
-    model = ConformalModel(model, calib_loader, alpha=0.1, lamda=lamda, constant_regularization=constant_regularization, randomized=randomized, allow_zero_sets=allow_zero_sets,kreg= kreg)
+    model = ConformalModel(model, calib_loader, alpha=0.1, lamda=lamda, constant_regularization=constant_regularization,
+                           randomized=randomized, allow_zero_sets=allow_zero_sets, kreg=kreg)
 
     print("Model calibrated and conformalized! Now evaluate over remaining data.")
     validate(val_loader, model, print_bool=True)
 
     print("Complete!")
+
+if __name__ == "__main__":
+    lamda = None
+    # define
+    kreg = None
+    # use the normal RAPS regularization
+    constant_regularization = True
+    main(kreg = kreg, constant_regularization = constant_regularization,lamda=lamda)
